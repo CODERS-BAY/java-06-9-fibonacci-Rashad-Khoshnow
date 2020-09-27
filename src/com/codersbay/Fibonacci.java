4591010c57fb830fdcbd5c92d60cdef9b40eedf5
Rashad Khoshnow <rashad.khoshnow@gmail.com>
2020-09-27 14:02:31 +0200

    Aufgabe erledigt

--- src/com/codersbay/Fibonacci.java
+++ src/com/codersbay/Fibonacci.java
@@ -2,8 +2,18 @@ package com.codersbay;
 
 public class Fibonacci {
 
+    public static int fib(int number) {
+        if (number == 0)
+            return 0;
+        else if (number == 1)
+            return 1;
+        else
+            return fib(number - 1) + fib(number - 2);
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello Fibonacci");
+        System.out.println(fib(6));
+
     }
 
 }
