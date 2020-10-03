82c399b2d18548e17c5d22bcf95416db8911aaa1
Rashad Khoshnow <rashad.khoshnow@gmail.com>
2020-10-03 16:39:33 +0200

    Geschwungene Klammern verwendet

--- src/com/codersbay/Fibonacci.java
+++ src/com/codersbay/Fibonacci.java
@@ -3,12 +3,13 @@ package com.codersbay;
 public class Fibonacci {
 
     public static int fib(int number) {
-        if (number == 0)
+        if (number == 0) {
             return 0;
-        else if (number == 1)
+        } else if (number == 1) {
             return 1;
-        else
+        } else {
             return fib(number - 1) + fib(number - 2);
+        }
     }
 
     public static void main(String[] args) {
